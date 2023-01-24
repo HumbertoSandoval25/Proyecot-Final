ce8cacf8079c63e27f3016496465fc384cd40e50
Humberto Sandoval <cobaltocode25@gmail.com>
2023-01-24 14:30:31 -0600

    Agregando base de datos de testimonios

---
+++ data/quotes.js
@@ -0,0 +1,42 @@
+const testQuotes = [
+    {
+        id: 1,
+        text:"Beautiful black ceramic watch, looks very fine and elegant. It was just a little tight for me, one more link in the bracelet would have been ideal, but it fits me and I will use it that way. Without a doubt, a perfect luxury that is worth it.",
+        name: "Joseph Murillo",
+        date: "January 02, 2021",
+        detail: "Director of a Company",
+        img: URL('https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80')
+    },
+    {
+        id: 2,
+        text:"The watch is great, the box well I feel that it lacks quality since it came as folded but from there the rest is very good.",
+        name: "Andy Flowers",
+        date: "February 14, 2021",
+        detail: "Lawyer",
+        img: URL('https://images.unsplash.com/photo-1485875437342-9b39470b3d95?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80')
+    },
+    {
+        id: 3,
+        text:"I liked it when saw it online at a glance. And thought it would be a bit chunky but on it arriving its a bit too much of school girl type of watch. Yet very elegant ladies watch if you are not looking for the Michael Kors type watches.",
+        name: "Robert M.",
+        date: "March 23, 2021",
+        detail: "Teacher",
+        img: URL('https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')
+    },
+    {
+        id: 4,
+        text:"It has a 16 cm cuff considering the extension it brings, it really is for very thin cuffs. In my case it didn't close, I needed 3 cm to make it fit and 4 to make it comfortable, I suggest that information be in the publication to avoid returns. For the rest, everything is perfect, it looks quality, elegant, equal to the advertising. Everything was received on time.",
+        name: "Felicia Henderson",
+        date: "July 14, 2021",
+        detail: "Marketin",
+        img: URL('https://images.unsplash.com/photo-1590649880765-91b1956b8276?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')
+    },
+    {
+        id: 5,
+        text:"It is very fragile, in itself it is beautiful, but if it falls it does not hold, just like with time the gold becomes ugly. For the time it lasted, I'm at a halfway point.",
+        name: "Humberto Sandoval",
+        date: "September 26, 2021",
+        detail: "Software Engineer",
+        img: URL('https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80')
+    }
+]
